6874f648e218a17c96d2fb8f633b15466f63aaa3
Nate Bruggeman <njbruggeman@gmail.com>
2022-03-03 10:24:52 -0600

    about section is pulling data successfully from sanity.io on 3333

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./About.scss";
-
+import { urlFor, client } from "../../client";
 import { images } from "../../constants";
 
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am a good developer",
-    imgURL: images.about01,
-  },
-  {
-    title: "Frontend",
-    description: "I know react and vue",
-    imgURL: images.about02,
-  },
-  {
-    title: "Backend",
-    description: "I know CSS and Sass",
-    imgURL: images.about03,
-  },
-];
+// const abouts = [
+//   {
+//     title: "Web Development",
+//     description: "I am a good developer",
+//     imgURL: images.about01,
+//   },
+//   {
+//     title: "Frontend",
+//     description: "I know react and vue",
+//     imgURL: images.about02,
+//   },
+//   {
+//     title: "Backend",
+//     description: "I know CSS and Sass",
+//     imgURL: images.about03,
+//   },
+// ];
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -41,7 +48,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgURL} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
