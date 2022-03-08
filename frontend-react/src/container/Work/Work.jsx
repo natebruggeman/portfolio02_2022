0c174264dede8d4a0bd10ce50ddf67266cdd9eb5
Nate Bruggeman <njbruggeman@gmail.com>
2022-03-08 11:09:48 -0600

    works section is rendering

--- frontend-react/src/container/Work/Work.jsx
+++ frontend-react/src/container/Work/Work.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import "./Work.scss";
 
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setactiveFilter] = useState("All");
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 });
+  const handleWorkFilter = (item) => {};
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">
+        My Creative
+        <span> Portfolio </span>
+        <br />
+        section
+      </h2>
+
+      {/* filtering through various projects */}
+      <div className="app__work-filter">
+        {["UI/UX", "Web App", "Mobile App", "React JS", "All"].map(
+          (item, index) => (
+            <>
+              <div
+                key={index}
+                onClick={() => handleWorkFilter(item)}
+                className={`app__work-filter app__flex p-text ${
+                  activeFilter === item ? "item-active" : ""
+                }`}
+              >
+                {item}
+              </div>
+
+              <motion.div
+                animate={animateCard}
+                transition={{ duration: 0.5, delayChildren: 0.5 }}
+                className="app__work-portfolio"
+              >
+                {filterWork.flatMap((work, index) => (
+                  <div className="app__work-item app__flex" key={index}>
+                    <div className="app__work-img app__flex">
+                      <img src={urlFor(work.imgUrl)} alt={work.name} />
+                    </div>
+                  </div>
+                ))}
+              </motion.div>
+            </>
+          )
+        )}
+      </div>
+    </>
+  );
 };
 
 export default Work;
