04e18d3f096c38286cb099d4c6ba893337a318c8
Nate Bruggeman <njbruggeman@gmail.com>
2022-03-09 11:57:50 -0600

    skills rendering and styled

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -27,7 +27,24 @@ const Skills = () => {
     <>
       <h2 className="head-text"> Skills & Experience</h2>
       <div className="app__skills-container">
-        <motion.div className="app__skills-list"></motion.div>
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div
+                className="app__flex"
+                style={{ backgroundColor: skill.bgColor }}
+              >
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
     </>
   );
