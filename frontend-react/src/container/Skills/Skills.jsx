35dd378bea7a11de098acde8d7e0327066666170
Nate Bruggeman <njbruggeman@gmail.com>
2022-03-09 11:25:19 -0600

    fetching skills and exp from sanity

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactTooltip from "react-tooltip";
+import { motion } from "framer-motion";
+
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { urlFor, client } from "../../client";
 import "./Skills.scss";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text"> Skills & Experience</h2>
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list"></motion.div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
