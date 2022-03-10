c1983089ec97ae9e3a5b9b73505a058aa556f1b6
Nate Bruggeman <njbruggeman@gmail.com>
2022-03-10 17:42:04 -0600

    added github link

--- frontend-react/src/components/SocialMedia.jsx
+++ frontend-react/src/components/SocialMedia.jsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { BsTwitter, BsLinkedin } from "react-icons/bs";
+import { BsTwitter, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => {
   const preventDefault = (event) => event.preventDefault();
 
   return (
     <div className="app__social">
+      <a href="https://github.com/natebruggeman" target="_blank">
+        <div>
+          <BsGithub />
+        </div>
+      </a>
       <a href="https://twitter.com/NateOnThursday" target="_blank">
         <div>
           <BsTwitter />
